f0312c3360cd0f08afd00c40f9135e04422c5219
Andrew Trenkal <trenkal.andrey@mail.ru>
2019-10-19 14:45:15 +0900

    implement Header component in project

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -2,66 +2,68 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './spinner.scss';
 
-export const Spinner = ({ size }) => {
-    return (
-        <div className={'preloader-main-wrapper'}>
-            <div className={`preloader-wrapper ${size} active`}>
-                <div className="spinner-layer spinner-blue">
-                    <div className="circle-clipper left">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="gap-patch">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="circle-clipper right">
-                        <div className="circle"></div>
-                    </div>
-                </div>
-
-                <div className="spinner-layer spinner-red">
-                    <div className="circle-clipper left">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="gap-patch">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="circle-clipper right">
-                        <div className="circle"></div>
-                    </div>
-                </div>
-
-                <div className="spinner-layer spinner-yellow">
-                    <div className="circle-clipper left">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="gap-patch">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="circle-clipper right">
-                        <div className="circle"></div>
-                    </div>
-                </div>
-
-                <div className="spinner-layer spinner-green">
-                    <div className="circle-clipper left">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="gap-patch">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="circle-clipper right">
-                        <div className="circle"></div>
-                    </div>
-                </div>
-            </div>
+const Spinner = ({ size }) => {
+  return (
+    <div className={'preloader-main-wrapper'}>
+      <div className={`preloader-wrapper ${size} active`}>
+        <div className="spinner-layer spinner-blue">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div>
+          <div className="gap-patch">
+            <div className="circle"></div>
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
         </div>
-    );
+
+        <div className="spinner-layer spinner-red">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div>
+          <div className="gap-patch">
+            <div className="circle"></div>
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
+        </div>
+
+        <div className="spinner-layer spinner-yellow">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div>
+          <div className="gap-patch">
+            <div className="circle"></div>
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
+        </div>
+
+        <div className="spinner-layer spinner-green">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div>
+          <div className="gap-patch">
+            <div className="circle"></div>
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Spinner.propTypes = {
-    size: PropTypes.oneOf(['big', 'small', '']),
+  size: PropTypes.oneOf(['big', 'small', '']),
 };
 
 Spinner.defaultProps = {
-    size: '',
+  size: '',
 };
+
+export default Spinner;
