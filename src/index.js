3401c234e02f99141598ccb448d60d2483c52025
Andrew Trenkal <trenkal.andrey@mail.ru>
2019-10-19 12:06:43 +0900

    install and included react-router-dom and materialize-css in project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './containers/app/App';
 import { Provider } from 'react-redux';
-import {store} from './store/configureStore'
+import { store } from './store/configureStore';
+import { Spinner } from './components/spinner/Spinner';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App>
+        <Suspense fallback={<Spinner />}>
+            <Route
+                exact
+                path={'/'}
+                component={lazy(() => import('./pages/home/Home'))}
+            />
+            <Route
+                path={'/employee/:id'}
+                component={lazy(() => import('./pages/employeeInfo/EmployeeInfo'))}
+            />
+        </Suspense>
+      </App>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
