cc02e1854168e392b2a99c21c9ceea5fd4b5104d
Andrew Trenkal <trenkal.andrey@mail.ru>
2019-10-19 16:19:10 +0900

    implement Employee container in Home page

--- src/containers/employees/Employees.jsx
+++ src/containers/employees/Employees.jsx
@@ -1,11 +1,28 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { getEmployees } from '../../actions/employees';
 
 class Employees extends PureComponent {
-    render() {
-        return (
-            <div>asdasd</div>
-        )
-    }
+  componentDidMount() {
+    const { getEmployees } = this.props;
+
+    getEmployees();
+  }
+
+  render() {
+    return <div>asdasd</div>;
+  }
 }
 
-export default Employees;
+const mapStateToProps = state => ({
+  employees: state.employees,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getEmployees: () => dispatch(getEmployees),
+});
+
+export default connect(
+  mapStateToProps,
+    mapDispatchToProps
+)(Employees);
